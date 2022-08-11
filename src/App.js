d518b0c78e99b1288bca2b582b85d158681d3eb2
Sam <bezzantsam@gmail.com>
2022-08-11 02:23:27 -0700

    class component Pet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+const Pet = (props) => {
+    return(
+        React.createElement("div", {}, [ 
+          React.createElement("h1", {}, props.name),  
+          React.createElement("h2", {}, props.animal),
+          React.createElement("h2", {}, props.breed),
+        ])
+    )
+}
+
+
 const App = () => {
     return React.createElement(
       "div", 
-      {},
-      React.createElement("h1", {id: "brand"}, "Adopt Me!")
-    );
+      {},[
+
+      
+      React.createElement("h1", {}, "Adopt Me!"),
+
+      React.createElement(Pet,{
+        name: "Luna", 
+        animal: "dog", 
+        breed: "Chihuahua"
+      }),
+      React.createElement(Pet,{
+        name: "Jim", 
+        animal: "dog", 
+        breed: "Chihuahua"
+      }),
+      React.createElement(Pet,{
+        name: "Bob", 
+        animal: "cat", 
+        breed: "Manx"
+      }),
+      ]
+    )
+   
   };
     ReactDOM.render(React.createElement(App), document.getElementById("root")); 
