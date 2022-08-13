1f1067bd166bca0d619d63042586532b913d0686
Sam <bezzantsam@gmail.com>
2022-08-13 01:53:13 -0700

    parcel

---
+++ dist/index.9e7b1db9.js
@@ -0,0 +1,36 @@
+const Pet = (props)=>{
+    return React.createElement("div", {
+    }, [
+        React.createElement("h1", {
+        }, props.name),
+        React.createElement("h2", {
+        }, props.animal),
+        React.createElement("h2", {
+        }, props.breed), 
+    ]);
+};
+const App = ()=>{
+    return React.createElement("div", {
+    }, [
+        React.createElement("h1", {
+        }, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "dog",
+            breed: "Chihuahua"
+        }),
+        React.createElement(Pet, {
+            name: "Jim",
+            animal: "dog",
+            breed: "Chihuahua"
+        }),
+        React.createElement(Pet, {
+            name: "Bob",
+            animal: "cat",
+            breed: "Manx"
+        }), 
+    ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+//# sourceMappingURL=index.9e7b1db9.js.map
