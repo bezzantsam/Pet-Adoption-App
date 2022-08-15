ab4c12f37b86dfb6ab72ecb76f96a45957ec353b
Sam <bezzantsam@gmail.com>
2022-08-15 10:40:03 -0700

    Details, router install/import

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Routes, RouterContext } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import { StictMode } from "react";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div id="my-app">
-      <h1> Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <div id="my-app">
+        <h1> Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </StrictMode>
   );
 };
 
---
+++ src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h2> Party</h2>;
+};
+
+export default Details;
